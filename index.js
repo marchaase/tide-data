25f7b8e7a30c5cc1bb5bed6a67e3586f10bda3f9
Marc Haase <marchaase@gmail.com>
2020-08-31 17:43:05 -0700

    add code for making NOAA tide requests

--- index.js
+++ index.js
@@ -1,11 +1,88 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const got = require('got');
 
+class TideData {
+    constructor() {
+        this.rawData = []
+
+        this.downloadData();
+    }
+
+    async downloadData() {
+        // https://tidesandcurrents.noaa.gov/api-helper/url-generator.html
+        // https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=20200101&end_date=20200131&station=9410170&product=water_level&datum=MTL&time_zone=gmt&units=english&format=json
+        // limited to 31 days max per request
+
+        let startDate = new Date('2020-01-01T00:00:00.000Z');
+        let endDate = new Date('2020-12-31T23:59:59.999Z');
+        let daysIncrement = 1;
+
+        let reqStartDate = startDate;
+        while(reqStartDate < endDate) {
+
+            // compute end date for request
+            let reqEndDate = new Date(reqStartDate);
+            reqEndDate.setDate(reqStartDate.getDate() + daysIncrement);
+            if (reqEndDate > endDate) reqEndDate = endDate;
+
+            console.log(`requesting data for ${reqStartDate} - ${reqEndDate}`);
+
+            let url = 'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=20200101&end_date=20200131&station=9410170&product=water_level&datum=MTL&time_zone=gmt&units=english&format=json';
+            let request = got(url, {
+                responseType: 'json'
+            }).then((res) => {
+                this.parseData(res.body)
+            }).catch((err) => {
+                console.log('ERROR: ');
+                console.log(err);
+            });
+            await request; // force synchronous to ensure async requests come back in order
+
+            reqStartDate = reqEndDate;
+        }
+    }
+
+    parseData(data) {
+        // TODO: assuming all data in order already, just append to existing storage
+
+        // expecting format:
+        // {
+        //     metadata: {
+        //         id: '9410170',
+        //         name: 'San Diego, San Diego Bay',
+        //         lat: '32.7142',
+        //         lon: '-117.1736'
+        //     },
+        //     data: [
+        //         {
+        //             t: '2020-01-01 00:00',
+        //             v: '-0.531',
+        //             s: '0.033',
+        //             f: '0,0,0,0',
+        //             q: 'v'
+        //         },
+        //         ...
+        //     ]
+        // }
+    }
+
+}
 
 class Main {
     constructor(listenPort) {
-        this.app = express();
         this.listenPort = listenPort;
+
+        this.app = express();
+        this.tideData = new TideData();
+
+        this.setupExpress();
         this.setupRoutes();
+    }
+
+    setupExpress() {
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+        this.app.use(bodyParser.json());
     }
 
     setupRoutes() {
